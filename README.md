8ee3589fbb2bd1df6b105de647e58ad08ebb38f0
Somilg11 <gsomil93@gmail.com>
2024-06-30 16:57:29 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+# staple-todo (manage your tasks) | React & TailwindCSS
+
+![Demo App](/public/staple_screenshot.png)
+
+-   🌟 Learn React + Tailwind 
+-   🎃 React Icons + Framer motion
+-   🚀 Seemless UI Experience
+
+# Run Locally
+
+### Install dependencies
+
+```shell
+npm install react-icons framer-motion
+```
+
+### Start the app
+
+```shell
+npm run dev
+```
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,8 +4,8 @@ function Background() {
   return (
     <div>
         <div className='fixed z-2 w-full h-screen'>
-        <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 font-semibold text-xl'>Documents.</div>
-        <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-bold text-zinc-900'>Docs.</h1>
+        <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 font-semibold text-xl'>staple.</div>
+        <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-bold text-zinc-900'>tasks</h1>
         </div>
     </div>
   )
